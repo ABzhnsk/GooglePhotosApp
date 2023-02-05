c7300b9c06cdb8a582ba951c6704a9e9975b6c14
ABzhnsk <96691110+ABzhnsk@users.noreply.github.com>
2023-02-05 19:01:09 +0300

    create searchVC

--- GooglePhotosApp/GooglePhotosApp/SearchViewController/SearchViewController.swift
+++ GooglePhotosApp/GooglePhotosApp/SearchViewController/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  GooglePhotosApp
 //
 //  Created by Anna Buzhinskaya on 05.02.2023.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
